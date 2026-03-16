709fff4be69dc3418830d94b82056e4f42cc5896
mr90733 <mr90733@mail.harpercollege.edu>
2026-03-15 19:46:09 -0500

    Part 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,65 @@
 #include <iostream>
 
-#include "src/hello.hpp"
+#include <fstream>
 
-int main() {
-  std::cout << hello() << std::endl;
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+    string filePath;
+
+    cout << "Enter the path to the file: ";  // Type old_faithful.csv
+
+    cin >> filePath;
+
+    ifstream file(filePath);
+
+    if (!file)
+
+        {
+        cout << "Error opening file" << endl;
+
+        return 1;
+    }
+
+    string header;
+
+    getline(file, header);
+
+    double eruption;
+
+    double waiting;
+
+    char comma;
+
+    double totalEruption = 0;
+
+    double totalWaiting = 0;
+
+    int count = 0;
+
+    while (file >> eruption >> comma >> waiting)
+
+        {
+        totalEruption = totalEruption + eruption;
+
+        totalWaiting = totalWaiting + waiting;
+
+        count = count + 1;
+    }
+
+    double avgEruption = totalEruption / count;
+
+    double avgWaiting = totalWaiting / count;
+
+    cout << "The average eruption length is " << avgEruption << " minutes." << endl;
+
+    cout << "The average eruption wait time is " << avgWaiting << " minutes." << endl;
+
+    file.close();
+
+    return 0;
 }
